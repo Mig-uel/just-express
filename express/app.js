eb96990336a72d253417a5286ccef1126009fd5d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-21 23:09:35 -0500

    Add static file serving and improve homepage response
    - Serve static files from the 'public' directory
    - Update homepage response to send 'index.html'
    - Add a catch-all route for non-existent pages

--- express/app.js
+++ express/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 /**
  * express is whatever was exported by the express module
  */
@@ -9,19 +10,28 @@ const express = require('express')
 const app = express()
 
 /**
+ * serve up static files
+ */
+app.use(express.static('public'))
+
+/**
  * all() method takes two args:
  * 1. route
  * 2. callback to run if the route is requested
  */
-app.all('*', (req, res) => {
+app.all('/', (req, res) => {
   /**
    * express handles the basic headers (status code, mime-type)
    */
-  res.send(`<h1>This is the homepage</h1>`)
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 
   /**
    * express handles the end, no need for res.end()
    */
+})
+
+app.all('*', (req, res) => {
+  res.send('<h1>Sorry, this page does not exist!</h1>')
 })
 
 app.listen(3000, () => console.log('SERVER LISTENING ON PORT 3000'))
