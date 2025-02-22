58192da1d1253a98fad6fdfdd985a553ad7577aa
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-21 20:37:05 -0500

    Enhance comments and update response message in HTTP server
    - Improved comments for clarity on the http module and its methods
    - Updated comments to be more descriptive
    - Added console log for server status on startup

--- http/app.js
+++ http/app.js
@@ -1,25 +1,32 @@
-// http module is native to Node
+/**
+ * http module is native to Node
+ */
 const http = require('http')
 
-// the http module has a createServer method
-// the createServer method takes in 1 argument, a callback function
-// the callback function takes 2 arguments: request and response
+/**
+ * the http module has a createServer method
+ * createServer takes in 1 argument: callback function
+ * the callback function takes 2 arguments: request and response
+ *
+ * the req argument contains data about the client
+ * the res argument is the response we send back
+ *
+ * the http message has:
+ * 1. start-line (we don't have to deal with, node will) - CHECK
+ * 2. header (we will deal with it)
+ * 3. body - (we write the body)
+ */
 const server = http.createServer((req, res) => {
-  // the req argument contains data about the client
-  // the res argument is the response we send back
-  //
-  // res object is our way to respond to the request
-  // the http message has:
-  // 1. start-line (we don't have to deal with, node will) - CHECK
-  // 2. header (we will deal with it)
-  // 3. body - (we write the body)
-  //
-  // writeHead(): takes 2 args, status code and the object for the mime-type
+  /**
+   * writeHead(): takes 2 args, status code and the object for the mime-type
+   */
   res.writeHead(200, { 'content-type': 'text/html' })
-  res.write('<h1>hello, world!</h1>')
+  res.write('<h1>This is the home page</h1>')
   res.end()
 })
 
-// createServer method returns an object with a listen method that takes in 1 argument:
-// port to listen for http traffic
-server.listen(3000)
+/**
+ * createServer method returns an object with a listen method that takes in 1 argument:
+ * port to listen for http traffic
+ */
+server.listen(3000, () => console.log('SERVER RUNNING'))
