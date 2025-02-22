198d0607aa4ba7e67c5bab23ff080e1df4dd5562
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-21 20:25:15 -0500

    Add response handling in HTTP server implementation
    - Implement response headers and body for client requests
    - Send a simple HTML message as a response

--- http/app.js
+++ http/app.js
@@ -7,8 +7,17 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   // the req argument contains data about the client
   // the res argument is the response we send back
-
-  console.log(req)
+  //
+  // res object is our way to respond to the request
+  // the http message has:
+  // 1. start-line (we don't have to deal with, node will) - CHECK
+  // 2. header (we will deal with it)
+  // 3. body - (we write the body)
+  //
+  // writeHead(): takes 2 args, status code and the object for the mime-type
+  res.writeHead(200, { 'content-type': 'text/html' })
+  res.write('<h1>hello, world!</h1>')
+  res.end()
 })
 
 // createServer method returns an object with a listen method that takes in 1 argument:
