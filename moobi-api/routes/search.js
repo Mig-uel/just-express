fef43ff73b2741ada4898e9ff697436949937831
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 16:02:08 -0500

    Add search functionality for movies and people - Implement search query validation - Filter movies and people based on search terms

--- moobi-api/routes/search.js
+++ moobi-api/routes/search.js
@@ -1,8 +1,40 @@
 const router = require('express').Router()
+const movies = require('../data/movies')
+const people = require('../data/people')
 
-/* GET home page. */
-router.get('/', function (req, res) {
-  return res.json({ msg: 'index hit' })
+router.use((req, res, next) => {
+  const { search } = req.query
+
+  if (!search)
+    return res.status(400).json({ msg: 'Search and/or type params required' })
+
+  return next()
+})
+
+router.get('/movie', function (req, res) {
+  const { search } = req.query
+
+  const results = movies.filter((movie) => {
+    const found =
+      movie.overview.toLowerCase().includes(search.toLowerCase()) ||
+      movie.title.toLowerCase().includes(search.toLowerCase())
+
+    return found
+  })
+
+  return res.json({ results })
+})
+
+router.get('/person', function (req, res) {
+  const { search } = req.query
+
+  const results = people.filter((person) => {
+    const found = person.name.toLowerCase().includes(search.toLowerCase())
+    
+    return found
+  })
+
+  return res.json({ results })
 })
 
 module.exports = router
