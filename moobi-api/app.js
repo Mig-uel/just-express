123032e3ada52b2cd53c5ce78eedea170c56a269
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 13:02:58 -0500

    Remove users route and simplify error handling
    - Deleted the unused users route to streamline the API
    - Updated error handling middleware to remove unnecessary parameters

--- moobi-api/app.js
+++ moobi-api/app.js
@@ -4,7 +4,6 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 
 var app = express()
 
@@ -14,7 +13,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -22,7 +20,7 @@ app.use(function (req, res, next) {
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
