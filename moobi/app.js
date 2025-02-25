6219ce79a87ab1293ce70e437705acf61ce5152d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 07:28:55 -0500

    Add initial Express app setup with views and styles
    - Set up Express application with helmet for security
    - Include basic CSS styles for layout and design
    - Add EJS views for head, index, navbar, and single movie pages

---
+++ moobi/app.js
@@ -0,0 +1,10 @@
+const helmet = require('helmet')
+const express = require('express')
+
+const app = express()
+
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000, () => console.log('SERVER STARTED'))
