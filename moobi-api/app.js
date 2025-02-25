ff6f2477a8dfd3c5c896c49a3eb5046214f5af12
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 13:05:12 -0500

    Add movie and search routers to main application
    - Integrate movieRouter for handling movie-related routes
    - Integrate searchRouter for handling search-related routes
    - Ensure cookieParser is applied correctly

--- moobi-api/app.js
+++ moobi-api/app.js
@@ -4,15 +4,19 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 
 var indexRouter = require('./routes/index')
+const movieRouter = require('/routes/movieRouter')
+const searchRouter = require('/routes/searchRouter')
 
 var app = express()
 
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
+app.use('/movie', movieRouter)
+app.use('/search', searchRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
