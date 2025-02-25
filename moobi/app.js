8294ddaa982ed0828b2d71985a8f9fb88f84a94b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 08:04:37 -0500

    Fetch now playing movies and handle errors in app - Update API endpoints for fetching now playing movies - Add error handling middleware for access denied errors - Set image base URL in response locals for views

--- moobi/app.js
+++ moobi/app.js
@@ -1,20 +1,48 @@
 const helmet = require('helmet')
 const express = require('express')
+require('express-async-errors')
 
 const app = express()
-const BASE_URL = 'https://api.themoviedb.org/3/find/4?external_source=imdb_id'
-const API_KEY = '0fe019cac089261188ecf06fc8aa1bfe'
+const BASE_URL = 'https://api.themoviedb.org/3'
+const NOW_PLAYING_URL = `${BASE_URL}/movie/now_playing?language=en-US`
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500'
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZmUwMTljYWMwODkyNjExODhlY2YwNmZjOGFhMWJmZSIsIm5iZiI6MTcxNzYyNjE4Ny41Mywic3ViIjoiNjY2MGU1NGJlNDM1MzYzNDc5NGY2ODIyIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.66i5lEGCn9d9TwdKYSeWt1qUNsgNO_kLgZaaTu-vOXk'
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-app.use(helmet())
+// app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+app.use((req, res, next) => {
+  res.locals.imageBase = IMAGE_BASE_URL
+  return next()
+})
+
 app.get('/', async (req, res) => {
-  return res.render('index')
+  const response = await fetch(NOW_PLAYING_URL, {
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  })
+  const data = await response.json()
+
+  return res.render('index', {
+    movies: data.results,
+  })
+})
+
+app.use((err, req, res, next) => {
+  if (err.message === 'access denied') {
+    res.status(403)
+    res.json({ error: err.message })
+  }
+
+  return next(err)
 })
 
 app.listen(3000, () => console.log('SERVER STARTED'))
