16fc1f8e263d7a0f96f068a56b8e6dba743c72f7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-25 07:40:59 -0500

    Set up Express app with EJS views and static files
    - Configure EJS as the view engine
    - Serve static files from the 'public' directory
    - Update view includes for consistency
    - Change title in index.ejs for branding

--- moobi/app.js
+++ moobi/app.js
@@ -2,9 +2,19 @@ const helmet = require('helmet')
 const express = require('express')
 
 const app = express()
+const BASE_URL = 'https://api.themoviedb.org/3/find/4?external_source=imdb_id'
+const API_KEY = '0fe019cac089261188ecf06fc8aa1bfe'
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
 app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', async (req, res) => {
+  return res.render('index')
+})
 
 app.listen(3000, () => console.log('SERVER STARTED'))
