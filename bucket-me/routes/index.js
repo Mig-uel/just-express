fb09fc7e3a595f9634d6441fdb54152d729edd30
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-02-26 04:10:31 -0500

    Add async support for signed URL generation route
    - Update the '/get-signed-url' route to handle asynchronous
      operations for generating S3 signed URLs.
    - Integrate the getS3SignedLink helper to return the signed
      URL along with the file metadata.

--- bucket-me/routes/index.js
+++ bucket-me/routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const mime = require('mime-types')
+const getS3SignedLink = require('../helpers/getS3SignedURL')
 
 /**
  * Purpose of this route:
@@ -8,7 +9,7 @@ const mime = require('mime-types')
  * 2. if so, ask aws for a signed url that can upload
  * 3. send that link back and provide feedback to the user
  */
-router.post('/get-signed-url', (req, res) => {
+router.post('/get-signed-url', async (req, res) => {
   const { fileName, fileSize, fileType } = req.body
 
   // encodeURIComponent - makes the name url safe, for instance, " " becomes %20
@@ -18,7 +19,15 @@ router.post('/get-signed-url', (req, res) => {
   const mimeType = mime.lookup(fileName)
 
   // call getS3PutLink and send it our uniqueFileName
-  return res.json(req.body)
+  const signedLink = await getS3SignedLink(uniqueFileName, mimeType)
+
+  console.log(signedLink)
+
+  return res.json({
+    signedLink,
+    mimeType,
+    uniqueFileName,
+  })
 })
 
 module.exports = router
